610846cf4f0761a889e00fb1e6fdc9538b079068
Bahaa Hani <57040462+bahaahani@users.noreply.github.com>
2024-02-28 00:00:50 +0300

    Add PATCH and DELETE methods to TasksController and implement updateTaskStatus and deleteTask in TasksService

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,15 @@
 import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
-import { Controller, Body, Get, Post, Param } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Get,
+  Post,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 
 @Controller('tasks')
 export class TasksController {
@@ -17,5 +26,18 @@ export class TasksController {
   @Get(':id')
   getTaskById(@Param('id') id: string) {
     return this.tasksService.getTaskById(id);
+  }
+
+  @Patch(':id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ) {
+    return this.tasksService.updateTaskStatus(id, status);
+  }
+
+  @Delete(':id')
+  deleteTask(@Param('id') id: string) {
+    return this.tasksService.deleteTask(id);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -23,6 +23,11 @@ export class TasksService {
   getTaskById(id: string): Task {
     return this.tasks.find((task) => task.id === id);
   }
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
   deleteTask(id: string): void {
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
