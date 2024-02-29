42d810f85f978ec540f3dceb29541dfdb30cc96a
Bahaa Hani <57040462+bahaahani@users.noreply.github.com>
2024-02-29 22:46:39 +0300

    Refactor code and update dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
-import { TasksService } from './tasks/tasks.service';
+
 @Module({
-  imports: [TasksModule],
-  controllers: [AppController],
-  providers: [AppService, TasksService],
+  imports: [
+    TypeOrmModule.forRoot(),
+    TasksModule,
+  ],
 })
 export class AppModule {}
